6727c8a2f7a2c939783c854bb9182d7df924c13a
Lyre <yvan.fedaouche@gmail.com>
2026-08-28 03:59:48 +0200

    fix(kernel): Removing useless code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,17 +1,9 @@
 #include "kernel/memory_manager/mmu.h"
 
 #define UART0_FIFO                ((volatile uint8_t *)0x60000000)
-#define UART0_STATUS_REG   (*(volatile uint32_t *)0x6000001C)
-
-#define UART_TXFIFO_CNT_SHIFT 16
-#define UART_TXFIFO_CNT_MASK  0xFF
 
 void uart_putchar(char c) {
     *UART0_FIFO = c;
-
-    for (volatile int i = 0; i < 1000; i++) {
-        __asm__ volatile ("nop");
-    }
 }
 
 void uart_print(const char *str) {
@@ -23,60 +15,6 @@ void uart_print(const char *str) {
         uart_putchar(*str);
         str++;
     }
-}
-
-void uart_print_size(size_t n) {
-    if (n == 0) {
-        uart_putchar('0');
-        return;
-    }
-
-    char buffer[21];
-    int i = 0;
-
-    while (n > 0) {
-        buffer[i++] = '0' + (n % 10);
-        n /= 10;
-    }
-
-    while (i > 0) {
-        uart_putchar(buffer[--i]);
-    }
-
-    uart_putchar('\r');
-    uart_putchar('\n');
-}
-
-void *memcpy(void *dest, const void *src, size_t n) {
-    unsigned char *d = dest;
-    const unsigned char *s = src;
-    while (n--) {
-        *d++ = *s++;
-    }
-    return dest;
-}
-
-void *memset(void *s, int c, size_t n) {
-    unsigned char *p = s;
-    while (n--) {
-        *p++ = (unsigned char)c;
-    }
-    return s;
-}
-
-void uart_print_hex(uint32_t n) {
-    uart_print("0x");
-    char hex_chars[] = "0123456789ABCDEF";
-    char buffer[8];
-    // On extrait les 8 nibbles (32 bits)
-    for (int i = 0; i < 8; i++) {
-        buffer[7 - i] = hex_chars[n & 0xF];
-        n >>= 4;
-    }
-    for (int i = 0; i < 8; i++) {
-        uart_putchar(buffer[i]);
-    }
-    uart_print("\n");
 }
 
 void kernel(void) {
--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ rm -f ${KERNEL_ELF}
 
 echo "[2/3] Compilation du kernel"
 
-${CC} -nostdlib -T ${LINKER_SCRIPT} -g -fno-tree-loop-distribute-patterns -mtext-section-literals -mlongcalls -fno-builtin -fno-stack-protector -fno-pic -fno-pie \
+${CC} -nostdlib -T ${LINKER_SCRIPT} -g -fno-tree-loop-distribute-patterns -mtext-section-literals -mlongcalls -fno-builtin -fno-stack-protector -fno-pic -fno-pie -Wall -Wextra -Werror -std=c99 -pedantic \
     -mabi=call0 \
     ${INCLUDES} \
     -o ${KERNEL_ELF} \
